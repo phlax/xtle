ae1e1632e484f9a7aa72a00edfec56860a3b7959
Ryan Northey <ryan@synca.io>
2020-04-04 17:06:17 +0100

    Add xtle.comment

---
+++ src/xtle/comment/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Xtle contributors.
+#
+# This file is a part of the Xtle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+default_app_config = 'xtle.comment.apps.XTLECommentConfig'
+
+
+def get_model():
+    from xtle.comment.models import Comment
+    return Comment
